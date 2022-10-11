92efee27c28fa61dd16733a6b307e60c0c90dedd
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-11 15:51:27 +0200

    initial

---
+++ src/hu/petrik/javaautok/Porsche.java
@@ -0,0 +1,20 @@
+package hu.petrik.javaautok;
+
+public class Porsche implements Auto{
+
+    private double sebesseg;
+
+    public Porsche(double sebesseg) {
+        this.sebesseg = sebesseg;
+    }
+
+    @Override
+    public void gyorsul() {
+        this.sebesseg += 10;
+    }
+
+    @Override
+    public double getSebesseg() {
+        return this.sebesseg;
+    }
+}
---
+++ src/hu/petrik/javaautok/Tesla.java
@@ -0,0 +1,23 @@
+package hu.petrik.javaautok;
+
+import java.util.Random;
+
+public class Tesla implements Auto{
+
+    private double sebesseg;
+    Random rnd = new Random();
+
+    public Tesla(double sebesseg) {
+        this.sebesseg = rnd.nextDouble() * 61;
+    }
+
+    @Override
+    public void gyorsul() {
+        this.sebesseg += rnd.nextDouble() * 11 + 5;
+    }
+
+    @Override
+    public double getSebesseg() {
+        return this.sebesseg;
+    }
+}
